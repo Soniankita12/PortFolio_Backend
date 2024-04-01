2d2eeb032906f89520afe3a1153d44e7b43e247a
soniankita12 <ankitasoni2211104@gmail.com>
2024-04-01 11:13:55 +0530

    update intro and about

--- routes/portfolioRoutes.js
+++ routes/portfolioRoutes.js
@@ -6,8 +6,8 @@ const {
   Experience,
   Contact,
 } = require("../Models/portfolioModel");
-
-router.get('/get-portfolio-data', async (req, res) => {
+// get all portfolio data
+router.get("/get-portfolio-data", async (req, res) => {
   try {
     const intros = await Intro.find();
     const abouts = await About.find();
@@ -20,6 +20,42 @@ router.get('/get-portfolio-data', async (req, res) => {
       project: projects,
       experience: experiences,
       contact: contacts[0],
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//update intro
+router.post("/update-intro", async (req, res) => {
+  try {
+    const intro = await Intro.findOneAndUpdate({ _id: req.body._id }, req.body, {
+      new: true,
+    });
+    res.status(200).send({
+      data: intro,
+      success: true,
+      message: "intro is updadated successfully",
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//update about
+router.post("/update-about", async (req, res) => {
+  try {
+    const about = await About.findOneAndUpdate(
+      { _id: req.body._id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).send({
+      data: about,
+      success: true,
+      message: "about is updadated successfully",
     });
   } catch (error) {
     res.status(500).send(error);
