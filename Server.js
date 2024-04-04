2a4bcc0d54f1ffaae1553efacee50f9a0d928d2a
soniankita12 <ankitasoni2211104@gmail.com>
2024-04-04 23:21:25 +0530

    login

--- Server.js
+++ Server.js
@@ -7,6 +7,12 @@ const portfolio = require('./routes/portfolioRoutes');
 app.use(express.json());
 app.use('/api/portfolio',portfolio)
 const Port =process.env.PORT || 5000;
+const path = require("path");
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(path.join(__dirname,"client/build")));
+    app.get("*",(req,res)=>{res.sendFile(path.join(__dirname,"client/build/index.html"))});
+}
 app.listen(Port,()=>{
     console.log(`app is listening on ${Port}`);
 })
